c42e8de4ac04dbf42c6aa268a084d5a78871f9d2
rjruiz-dev <rjruizsf@gmail.com>
2026-04-04 11:25:32 -0300

    feat(layout): integra componentes shared en main layout con imports desde barrel

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -5,5 +5,8 @@
  */
 
 export * from './badge/badge.component';
+export * from './error-alert/error-alert.component';
+export * from './footer/footer.component';
+export * from './loading-spinner/loading-spinner.component';
 export * from './navbar/navbar.component';
 export * from './product-card/product-card.component';
